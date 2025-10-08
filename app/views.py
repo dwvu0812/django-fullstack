1de9c1797db6c816692193a219b75722f5d8189c
Khanh Du Vu <khanhdu1211@gmail.com>
2025-10-09 05:27:37 +0700

    feat: Implement article creation view and template, update URLs for article creation

--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.forms import CreateArticleForm
 from app.models import Article
 
 
 def home(request):
     articles = Article.objects.all()
     return render(request, "app/home.html", {"articles": articles})
+
+
+def create_article(request):
+    if request.method == "POST":
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            new_article = Article.objects.create(
+                title=form_data["title"],
+                content=form_data["content"],
+                word_count=form_data["word_count"],
+                twitter_post=form_data["twitter_post"],
+                status=form_data["status"],
+            )
+            new_article.save()
+            return redirect("home")
+    else:
+        form = CreateArticleForm()
+    return render(request, "app/article_create.html", {"form": form})
