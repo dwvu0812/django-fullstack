4cd63381b54a5175c89241f4fb21df4961c51890
Khanh Du Vu <khanhdu1211@gmail.com>
2025-10-21 20:48:42 +0700

    feat: Add superuser creation to startup script for initial setup

--- start.sh
+++ start.sh
@@ -3,5 +3,11 @@
 # Run database migrations
 poetry run python manage.py migrate
 
+# Create superuser if it doesn't exist
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@example.com  
+export DJANGO_SUPERUSER_PASSWORD=admin
+poetry run python manage.py createsuperuser --noinput || true
+
 # Start the Django development server
 poetry run python manage.py runserver 0.0.0.0:8000
